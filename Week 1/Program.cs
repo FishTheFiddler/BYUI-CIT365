5c270a19d1b81973fb2ae72a6906e87c42ff3654
FishTheFiddler <wesspam11@yahoo.com>
2022-04-19 16:26:16 -0600

    Create Program.cs

---
+++ Week 1/Program.cs
@@ -0,0 +1,57 @@
+using System;
+
+namespace CIT365_Week_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            string name;
+            string location;
+            DateTime today;
+            DateTime xmas = new System.DateTime(2022, 12, 25);
+            int daysTilXmas;
+
+            // Standard greeting for the program
+            Console.WriteLine("Welcome to the CIT365 Week 1 program.");
+
+            // Call our functions and assign the results to our variables
+            name = PromptName();
+            location = PromptLocation();
+            today = DateTime.Today;
+            daysTilXmas = DaysTilXmas(today, xmas);
+
+            // Display the final output
+            DisplayValues(name, location, today, daysTilXmas);
+        }
+
+        // Prompt the User for their name. (No Validation)
+        static String PromptName() 
+        {
+            Console.Write("Enter your Name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        // Prompt the User for their location. (No Validation)
+        static String PromptLocation()
+        {
+            Console.Write("Enter your Location: ");
+            string location = Console.ReadLine();
+            return location;
+        }
+        // Calculate how many days there are until Christmas.
+        static int DaysTilXmas(DateTime today, DateTime xmas) {
+            System.TimeSpan diff1 = xmas.Subtract(today);
+            return (int) diff1.Days;
+        }
+        // Display all the values once they have been assigned.
+        static void DisplayValues(string name, string location, DateTime date, int daysTilXmas) {
+            Console.WriteLine("\nYour name is " + name + ", And you are from " + location + ".");
+            Console.WriteLine("Today's date is " + date.ToString("d") + ".");
+            Console.WriteLine("And there are " + daysTilXmas + " days until Christmas.\n");
+            Console.WriteLine("Press Any Key to End Program...");
+            Console.ReadKey();
+        }
+    }
+}
