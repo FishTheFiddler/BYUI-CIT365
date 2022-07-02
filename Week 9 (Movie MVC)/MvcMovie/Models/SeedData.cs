ee928cb77c06cb40e2b08d671115ac3e6219619f
WesPeterson237 <wesspam11@yahoo.com>
2022-07-02 13:36:14 -0600

    updating files

---
+++ Week 9 (Movie MVC)/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The Best 2 Years",
+                        ReleaseDate = DateTime.Parse("2004-2-12"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Once I was a Beehive",
+                        ReleaseDate = DateTime.Parse("2015-3-13"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Other Side of Heaven",
+                        ReleaseDate = DateTime.Parse("2001-2-23"),
+                        Genre = "Spiritual",
+                        Rating = "PG",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Work and the Glory",
+                        ReleaseDate = DateTime.Parse("2004-4-15"),
+                        Genre = "Spiritual",
+                        Rating = "PG",
+                        Price = 3.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Saints and Soldiers",
+                        ReleaseDate = DateTime.Parse("2003-4-15"),
+                        Genre = "War",
+                        Rating = "PG-13",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
